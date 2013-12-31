f48c017924afb019cd099661f15e8e81c92b42eb
Yusuke Hosonuma <tobi462@gmail.com>
2014-01-01 01:40:37 +0900

    PlayFrameworkのバージョンを2.1.3に更新

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,14 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "min-fare"
+  val appName         = "MinFare"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean,
-    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+    javaEbean
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
